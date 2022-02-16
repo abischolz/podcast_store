0e2f5186f1ccb6a13b5302ef07cc171659319d12
abischolz <Abi.scholz@gmail.com>
2022-02-16 18:27:00 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  React.createElement('div', null, 'Hello React'),
+  // eslint-disable-next-line no-undef
+  document.getElementById('app')
+);
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'foo.bundle.js',
+  },
+  plugins: [new ESLintPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.(jsx|js)$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [['@babel/preset-env'], '@babel/preset-react'],
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
