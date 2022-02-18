9a669924af35394ff3d5c6996b5c51082ed33ae7
abischolz <Abi.scholz@gmail.com>
2022-02-18 10:46:43 -0500

    Created main app component. Added API packages and keys

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router } from 'react-router-dom';
+import App from './App';
+
+const { Client } = require('podcast-api');
+export const client = Client({ apiKey: process.env.PODCAST_API_KEY });
+
+// eslint-disable-next-line no-unused-vars
 
 ReactDOM.render(
-  React.createElement('div', null, 'Hello React'),
+  <Router>
+    <App />
+  </Router>,
   // eslint-disable-next-line no-undef
+  // eslint-disable-next-line comma-dangle
   document.getElementById('app')
 );
