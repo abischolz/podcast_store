7425f1e7a703f0eac8a368d59644646b04403057
abischolz <Abi.scholz@gmail.com>
2022-03-07 12:20:02 -0500

    Removed firebase - adding different auth

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Route} from react-router-dom;
+import HomeHeader from './Components/Header';
 
 function App() {
 
   return(
   <div>
+    <HomeHeader />
 <div>This is the app.</div>
+    
     <Route path="/login" component={Login}/>
     <Route path="/signup" component={SignUp} />
     <Route path="/home" component={Home} />
--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,7 +1,33 @@
-import React from 'react';
+/* eslint-disable react/function-component-definition */
+import React, { useState } from 'react';
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  TextField,
+} from '@mui/material';
+import { Link } from 'react-router-dom';
 
 const Login = (props) => {
-  return <div>this will be the login component</div>;
+  const [open, setOpen] = useState(false);
+
+  return (
+    <Dialog open={open}>
+      <DialogTitle>Log in!</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          Log in to access your saved preferences! Don't have an account?
+          <Link path="/signup">
+            Sign up and starting building your library!
+          </Link>
+        </DialogContentText>
+        <TextField label="Email Address" type="email" />
+
+        <TextField label="Password" />
+      </DialogContent>
+    </Dialog>
+  );
 };
 
 export default Login;
